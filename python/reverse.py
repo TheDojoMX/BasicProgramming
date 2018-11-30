12421b4751b65fb7b32fb005a47edc19bcfbc7c5
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-11-29 23:58:33 -0600

    reverse

---
+++ python/reverse.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+grupo = ['Nadia', 'Rocío', 'Valeria', 'Ángel', 'Frank', 'Heliane']
+# grupo[::-1]
+
+longitud = len(grupo)
+for i in range(longitud):
+    indice = longitud-i-1
+    print(grupo[i])
+
