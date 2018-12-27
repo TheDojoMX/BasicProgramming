6a322054da6d7b8bb2751b2aa6d7770ed4c631ab
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-12-26 23:56:59 -0600

    Caesar cypher

---
+++ python/caesar_cypher.py
@@ -0,0 +1,9 @@
+"""
+Crear 2 funciones
+1) Cifrar: toma una cadena de texto y un número N. Devuelve una cadena
+en dónde cada carácter alfabético lo "mueve" por N 
+posiciones en el alfabeto. Ejemplo:
+ cifrar("abc", 2) -> "cde": transformó la "a" en "c"
+2) descifrar: aplica la operación inversa, recibe una cadena cifrada y
+el número N y devuelve la cadena bien formada.
+"""
